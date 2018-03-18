33e2033708b32839d5777be4ffece91b41822fb4
Tyler Bowdish <tylerbowdish@cedarville.edu>
2018-03-18 14:44:43 -0400

    Finished FunctionDeclaration

--- CMinusCompiler/src/Compiler/Parser/ParseTokens/Declaration/FunctionDeclaration.java
+++ CMinusCompiler/src/Compiler/Parser/ParseTokens/Declaration/FunctionDeclaration.java
@@ -2,7 +2,9 @@ package Compiler.Parser.ParseTokens.Declaration;
 
 import Compiler.Parser.TokenList;
 import Compiler.Scanner.Token;
+import static Compiler.Scanner.Token.TokenType.*;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +14,37 @@ public class FunctionDeclaration extends Declaration{
     List<Parameter> params;
     CompoundStatement compoundStatement;
 
-    public static FunctionDeclaration parseFunctionDeclarationPrime(TokenList tokens, Token typeSpecifier, Token id) {
+    public FunctionDeclaration(Token typeSpecifier, Token id, List<Parameter> params, CompoundStatement compoundStatement){
+        this.typeSpecifier = typeSpecifier;
+        this.id = id;
+        this.params = params;
+        this.compoundStatement = compoundStatement;
+    }
+
+    public static FunctionDeclaration parseFunctionDeclarationPrime(TokenList tokens, Token typeSpecifier, Token id) throws ParseException {
         List<Parameter> params;
         CompoundStatement compoundStatement;
-        if()
+        if(tokens.getNextToken().match(OPEN_PARENS_TOKEN)){
+            Token nextToken = tokens.viewNextToken();
+            if(nextToken.match(VOID_TOKEN)){
+                params = null;
+            }
+            else if(nextToken.match(INT_TOKEN)){
+                params = Parameter.parseParamList(tokens);
+                if(tokens.getNextToken().match(CLOSE_PARENS_TOKEN)){
+                    compoundStatement = CompoundStatement.parseCompoundStatement(tokens);
+                    return new FunctionDeclaration(typeSpecifier, id, params, compoundStatement);
+                }
+                else{
+                    throw new ParseException("PARSE ERROR", 4);
+                }
+            }
+            else{
+                throw new ParseException("PARSE ERROR", 4);
+            }
+        }
+        else{
+            throw new ParseException("PARSE ERROR", 4);
+        }
     }
 }
