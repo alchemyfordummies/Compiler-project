78dd2632358ce0f928df9965cb1a9b025c73c92b
Tim <tfroberg@cedarville.edu>
2018-03-18 14:58:28 -0400

    Add DeclarationList

--- CMinusCompiler/src/Compiler/Parser/ParseTokens/Declaration/Declaration.java
+++ CMinusCompiler/src/Compiler/Parser/ParseTokens/Declaration/Declaration.java
@@ -1,43 +1,41 @@
 package Compiler.Parser.ParseTokens.Declaration;
+
 import Compiler.Parser.TokenList;
 import Compiler.Scanner.Token;
 
 import java.text.ParseException;
-import java.util.List;
+
 import static Compiler.Scanner.Token.TokenType.*;
 
 public class Declaration {
-    public Declaration parseDeclaration(TokenList tokens) throws ParseException{
+    public Declaration parseDeclaration(TokenList tokens) throws ParseException {
         Token typeSpecifier = tokens.getNextToken();
         Token idToken = tokens.getNextToken();
-        if(!(typeSpecifier.match(VOID_TOKEN) || typeSpecifier.match(INT_TOKEN)) || idToken.match(ID_TOKEN)){
+
+        if (!(typeSpecifier.match(VOID_TOKEN) || typeSpecifier.match(INT_TOKEN)) || idToken.match(ID_TOKEN)) {
             throw new ParseException("PARSE ERROR", 5);
         }
-        if(typeSpecifier.match(VOID_TOKEN)){
+        if (typeSpecifier.match(VOID_TOKEN)) {
             return FunctionDeclaration.parseFunctionDeclarationPrime(tokens, typeSpecifier, idToken);
-        }
-        else{
+        } else {
             return parseDeclarationPrime(tokens, typeSpecifier, idToken);
         }
     }
 
-    public Declaration parseDeclarationPrime(TokenList tokens, Token typeIdentifier, Token id) throws ParseException{
+    public Declaration parseDeclarationPrime(TokenList tokens, Token typeIdentifier, Token id) throws ParseException {
         Token currToken = tokens.getNextToken();
-        if(currToken.match(SEMICOLON_TOKEN)){
+        if (currToken.match(SEMICOLON_TOKEN)) {
             return new VarDeclaration(typeIdentifier, id);
-        }
-        else if(currToken.match(OPEN_BRACKET_TOKEN)){
+        } else if (currToken.match(OPEN_BRACKET_TOKEN)) {
             Token numToken = tokens.getNextToken();
-            if(!numToken.match(NUM_TOKEN)){
+            if (!numToken.match(NUM_TOKEN)) {
                 throw new ParseException("PARSE ERROR", 5);
             }
             return new VarDeclaration(typeIdentifier, id, numToken);
-        }
-        else if(currToken.match(OPEN_PARENS_TOKEN)){
+        } else if (currToken.match(OPEN_PARENS_TOKEN)) {
             tokens.ungetToken();
             return FunctionDeclaration.parseFunctionDeclarationPrime(tokens, typeIdentifier, id);
-        }
-        else{
+        } else {
             throw new ParseException("PARSE ERROR", 5);
         }
     }
--- CMinusCompiler/src/Compiler/Parser/ParseTokens/DeclarationList.java
+++ CMinusCompiler/src/Compiler/Parser/ParseTokens/DeclarationList.java
@@ -1,17 +1,31 @@
 package Compiler.Parser.ParseTokens;
 
+import Compiler.Parser.ParseTokens.Declaration.Declaration;
+import Compiler.Parser.TokenList;
 import Compiler.Scanner.Token;
-import Tree.TreeNode;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
-public class DeclarationList throws ParseException {
-    public TreeNode parseToken(ArrayList<Token> tokens, int index) {
-        return null;
-    }
+import static Compiler.Scanner.Token.TokenType.*;
 
-    @Override
-    public void match(Token token) {
+public class DeclarationList {
+    private ArrayList<Declaration> declarations = new ArrayList<>();
 
+    public ArrayList<Declaration> parseDeclarationList(TokenList tokens) throws ParseException {
+        Token nextToken = tokens.getNextToken();
+
+        while (nextToken.match(VOID_TOKEN) || nextToken.match(INT_TOKEN)) {
+            Declaration declaration = new Declaration();
+            // will this update the tokens for use in this file?
+            declarations.add(declaration.parseDeclaration(tokens));
+            nextToken = tokens.getNextToken();
+        }
+
+        if (declarations.size() == 0) {
+            throw new ParseException("PARSE ERROR", 5);
+        } else {
+            return declarations;
+        }
     }
 }
