1324f5261f300d3d077c6fab46b61a45ebab92e0
Tim <tfroberg@cedarville.edu>
2018-03-19 09:27:16 -0400

    Added Statement

--- CMinusCompiler/src/Compiler/Parser/ParseTokens/Statement/Statement.java
+++ CMinusCompiler/src/Compiler/Parser/ParseTokens/Statement/Statement.java
@@ -1,7 +1,39 @@
 package Compiler.Parser.ParseTokens.Statement;
 
 import Compiler.Parser.ParseTokens.ParseToken;
+import Compiler.Parser.TokenList;
+import Compiler.Scanner.Token;
 
-public abstract class Statement implements ParseToken {
+import java.text.ParseException;
 
+import static Compiler.Scanner.Token.TokenType.*;
+
+public class Statement {
+    public Statement parseStatement(TokenList tokens) throws ParseException {
+        Token nextToken = tokens.getNextToken();
+
+        if (nextToken.match(ID_TOKEN)) {
+            // ExpressionStatement expressionStatement = new ExpressionStatement();
+            // expressionStatement.parseExpressionStatement(tokens);
+            // return expressionStatement;
+        } else if (nextToken.match(OPEN_CURLY_BRACE_TOKEN)) {
+            // CompoundStatement compoundStatement = new CompoundStatement();
+            // compoundStatement.parseCompoundStatement(tokens);
+            // return compoundStatement;
+        } else if (nextToken.match(IF_TOKEN)) {
+            // SelectionStatement selectionStatement = new SelectionStatement();
+            // selectionStatement.parseSelectionStatement(tokens);
+            // return selectionStatement;
+        } else if (nextToken.match(WHILE_TOKEN)) {
+            // IterationStatement iterationStatement = new IterationStatement();
+            // iterationStatement.parseIterationStatement(tokens);
+            // return iterationStatement;
+        } else if (nextToken.match(RETURN_TOKEN)) {
+            // ReturnStatement returnStatement = new ReturnStatement();
+            // returnStatement.parseReturnStatement(tokens);
+            // return returnStatement;
+        } else {
+            throw new ParseException("Invalid token for statement", 8);
+        }
+    }
 }
